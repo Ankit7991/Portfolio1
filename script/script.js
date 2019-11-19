d1888187ab74f8720bbb89b468cbed351cdbfcce
Ankit7991 <m.ankitkumar001@gmail.com>
2019-11-20 00:21:42 +0530

    Removed Shining animation in languages

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-'use strict'; 
-window.onload = function(){
+'use strict';
+window.onload = function () {
   //check if element is in viewport [CLOSURE- JUST EXPERIMENTING]
   // function inViewport(parentId, elementId, animation = false){
 
@@ -11,7 +11,7 @@ window.onload = function(){
   //   if(!animation){
 
   //     return function(activateDistance, ifClassName, elseClassName, padding = false, padClass){
-        
+
   //       window.onscroll = function(){
   //         var height = parent.getBoundingClientRect().top;
   //         if(height <= activateDistance){
@@ -32,54 +32,72 @@ window.onload = function(){
 
 
   //auto type container
-  (function autoTypeContainer(){ 
+  (function autoTypeContainer() {
 
-    var about = [
+    // var about = [
+    var words = [
       // `Hello `,'',
       // `Ankit here`,'',
       // `I love (<3)`,'',
-      'HTML (****) ','',
-      'CSS (***) ','',
-      'AND JS (***) ','',
-    ]; 
-    var letterCount = 0;
-    var index = 0;
-    var totalWords = about.length ;
+      // 'HTML (****) ','',
+      // 'CSS (***) ','',
+      // 'AND JS (***) ','',
+      'Front end Developer'
+    ];
     var output = document.querySelector('#intro-typing');
-    var myInterval;
-    var intervalTime = 200;
-    
-    (function autoType(){
+    var myInterval, typedWord = '';
+    var letterIndex = 0,
+      wordIndex = 0;
+    (function typing() {
       clearInterval(myInterval);
-      var currentWord = about[index];
-      var slicedCurrent = currentWord.slice(0, letterCount);
-      output.innerHTML = slicedCurrent;
-      //interval time management
-      if(slicedCurrent.length >= about[index].length){
-        intervalTime = 1000;
-      }else{
-        intervalTime = 200;
-      }
+      let currentWord = words[wordIndex];
+      let currentLetter = currentWord[letterIndex];
+      typedWord += currentLetter;
       myInterval = setInterval(() => {
-        if(slicedCurrent.length === about[index].length){
-          index++;
-          letterCount = 0;
-          (index >= totalWords) ? index = 0 : false;
-        }
-        letterCount++;
-        autoType();
-      }, intervalTime);
+        output.innerHTML = typedWord;
+
+        letterIndex++;
+        // typing();
+      }, 300);
     })();
+    // var letterCount = 0;
+    // var index = 0;
+    // var totalWords = about.length ;
+    // var output = document.querySelector('#intro-typing');
+    // var myInterval;
+    // var intervalTime = 200;
+
+    // (function autoType(){
+    //   clearInterval(myInterval);
+    //   var currentWord = about[index];
+    //   var slicedCurrent = currentWord.slice(0, letterCount);
+    //   output.innerHTML = slicedCurrent;
+    //   //interval time management
+    //   if(slicedCurrent.length >= about[index].length){
+    //     intervalTime = 1000;
+    //   }else{
+    //     intervalTime = 200;
+    //   }
+    //   myInterval = setInterval(() => {
+    //     if(slicedCurrent.length === about[index].length){
+    //       index++;
+    //       letterCount = 0;
+    //       (index >= totalWords) ? index = 0 : false;
+    //     }
+    //     letterCount++;
+    //     autoType();
+    //   }, intervalTime);
+    // })();
   })();
 
   //sticky nav
   // (function stickyNav(){
-    
+
   //   //viewport closure
   //   // var stickyNav = inViewport('main', 'nav');
   //   // stickyNav(-100, 'sticky', 'nonsticky', true, 'sticky-nav-wrapper');
   //   //upper code is same as below commented code
-    
+
   //   // var navWrapper = document.querySelector('#main');
   //   // var nav = document.querySelector('#nav');
   //   // window.onscroll = function(){
@@ -96,15 +114,15 @@ window.onload = function(){
   // })();
 
   //active nav button
-  (function activNavButton(){
+  (function activNavButton() {
     var links = document.querySelectorAll('.navLink');
     var colors = ['#db7070', '#43c943', '#c48e48', '#e96de3']
     var nav = document.querySelector('#nav');
-    for(let i = 0; i < links.length; i++){
-      (function(i){
-        links[i].addEventListener('click', function(event){
+    for (let i = 0; i < links.length; i++) {
+      (function (i) {
+        links[i].addEventListener('click', function (event) {
           //reset every other elements
-          for(let i = 0; i < links.length; i++){
+          for (let i = 0; i < links.length; i++) {
             links[i].classList.remove('active');
           }
           //change only this element
@@ -117,47 +135,49 @@ window.onload = function(){
 
   })();
 
-  (function onscrollAll(){
+  (function onscrollAll() {
     //sticky nav variables
     var navWrapper = document.querySelector('#main');
     var nav = document.querySelector('#nav');
     //language animation function
-    function changeClass(element, updateTo){
+    function changeClass(element, updateTo) {
       var element = document.querySelector(`.${element}`);
       element.classList.add(updateTo);
     }
 
     var parent = document.querySelector('.bar-wrapper');
-    window.onscroll = function(){
+    window.onscroll = function () {
 
       //languages animation
       let windowHeight = window.innerHeight;
       let parentDistance = parent.getBoundingClientRect().top;
-      if(parentDistance <= (windowHeight/2)){
+      if (parentDistance <= (windowHeight / 2)) {
         changeClass('html-bar-value', 'html-bar-value-animate');
         changeClass('css-bar-value', 'css-bar-value-animate');
         changeClass('js-bar-value', 'js-bar-value-animate');
         changeClass('php-bar-value', 'php-bar-value-animate');
         changeClass('bootstrap-bar-value', 'bootstrap-bar-value-animate');
         changeClass('jquery-bar-value', 'jquery-bar-value-animate');
+        changeClass('ui-bar-value', 'ui-bar-value-animate');
+        changeClass('photoshop-bar-value', 'photoshop-bar-value-animate');
         // changeClass('title-value-wrapper', 'title-value-wrapper-animate');
         //title value wrappers -> tvWrappers
-        var languages = ['html', 'css', 'js', 'php', 'bootstrap', 'jquery' ];
+        var languages = ['html', 'css', 'js', 'php', 'bootstrap', 'jquery', 'ui', 'photoshop'];
         var tvWrappers = document.querySelectorAll('.title-value-wrapper');
         // for(let i = 0; i < tvWrappers.length; i++){
         //   tvWrappers[i].classList.add('title-value-wrapper-animate');
         // }
-        for(let i in languages){
+        for (let i in languages) {
           tvWrappers[i].classList.add(`${languages[i]}-value-wrapper-animate`)
         }
       }
 
       //sticky nav
       var height = navWrapper.getBoundingClientRect().top;
-      if(height <= -50){
+      if (height <= -50) {
         nav.className = 'sticky';
         navWrapper.classList.add('sticky-nav-wrapper');
-      }else{
+      } else {
         nav.className = 'nonSticky';
         navWrapper.classList.remove('sticky-nav-wrapper');
       }
@@ -165,12 +185,12 @@ window.onload = function(){
   })();
 
 
-  (function linkScroll(){
+  (function linkScroll() {
 
     //using jquery 
-    function scroll(nowId, toId, duration = 500, extraMargin){
-      $(`#${nowId}`).click(function(){
-        $(document).ready(function(){
+    function scroll(nowId, toId, duration = 500, extraMargin) {
+      $(`#${nowId}`).click(function () {
+        $(document).ready(function () {
           $('html, body').animate({
             scrollTop: $(`#${toId}`).offset().top
           }, duration)
